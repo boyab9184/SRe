c51f2d46b4a17945d8c3a989e04c6f097f5fba2b
benev <boyan.benev@gtmhub.com>
2022-06-09 06:55:40 +0200

    Refactor main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
 import export_to_file as ef
 
-print("\nWELCOME to Boyan Benev's codding assignment")
 
-while True:  
-    print("\nMAIN MENU:\n")  
 
-    user_choice = input("Enter the file path you want to be analysed or press 0 to exit:\n")
-    if user_choice == "0":
-        break 
-    else:
-        try:
-            print("\nYou can find the output file in:", ef.export_to_file(user_choice))
-            break
-        except FileNotFoundError:
-            print("Wrong file path. Please try again")
+def menu():
+
+    print("\nWELCOME to Boyan Benev's codding assignment")
+
+    while True:  
+        print("\nMAIN MENU:\n")  
+
+        user_choice = input("Enter the file directory path you want to be analysed or press 0 to exit:\n")
+        if user_choice == "0":
+            break 
+        else:
+            try:
+                print("\nYou can find the output file in:", ef.export_to_file(user_choice))
+                break
+            except FileNotFoundError:
+                print("Wrong file path. Please try again")
+
+
+if __name__ == "__main__":
+   menu()
